950288ff11d4da9fa82db65b1cef1e4339b7170a
suugaku0 <gengpingbanben4@gmail.com>
2026-05-16 12:51:55 +0900

    Update others.js

--- others.js
+++ others.js
@@ -2,5 +2,7 @@ const query = window.location.search
 const urlParams=new URLSearchParams(query)
 const id=urlParams.get("id")
 const com=urlParams.get("pagecom")
+const title = document.getElementById("titleText")
+title.textContent=`${com}会`
 import {request} from "./defoscript.js"
 request(com)
